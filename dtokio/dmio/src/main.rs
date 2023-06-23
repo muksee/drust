5824a4e738de35ce8e54f666cfc1acd4304cc471
root <root@MUKSEE.COM>
2023-06-23 19:44:21 +0800

    20230624

--- dtokio/dmio/src/main.rs
+++ dtokio/dmio/src/main.rs
@@ -1,3 +1,11 @@
+/*
+EPOLL:
+https://man7.org/linux/man-pages/man7/epoll.7.html
+MIO:
+https://docs.rs/mio
+https://github.com/tokio-rs/mio
+ */
+
 use mio::{
     net::{TcpListener, TcpStream},
     Events, Interest, Poll, Token,
@@ -6,21 +14,25 @@ use mio::{
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let addr = "127.0.0.1:0".parse()?;
     let server = TcpListener::bind(addr)?;
-
-    let mut poll = Poll::new()?;
-    let mut events = Events::with_capacity(1024);
-
     let mut stream = TcpStream::connect(server.local_addr()?)?;
 
-    poll.registry().register(
-        &mut stream,
-        Token(0),
-        Interest::READABLE | Interest::WRITABLE,
-    )?;
+    // EPOLL: epoll_crate(int size)
+    let mut poll = Poll::new()?;
+    // EPOLL: 已发生的事件缓存
+    let mut events = Events::with_capacity(1024);
 
-    println!("{}",events.is_empty());
+    // EPOLL: epoll_ctl(epfd, int op, int fd, epoll_event *event)
+    poll.registry()
+        .register(
+            &mut stream,
+            Token(0),
+            Interest::READABLE | Interest::WRITABLE,
+        )?;
+
+    println!("{}", events.is_empty());
 
     loop {
+        // EPOLL: int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
         poll.poll(&mut events, None)?;
 
         for event in &events {
